44388f58ec5396e7e3efc8b22f6a8f4f32fefacb
Antonio Fernando <96031071+antoniofernandodj@users.noreply.github.com>
2022-07-08 08:50:40 -0300

    My first commit
    File showing basic of commenting and printing

---
+++ ex001.lua
@@ -0,0 +1,16 @@
+-- This is the first file of repo. This is a
+-- line comment. Not so good to write long text
+
+--[[ but this is a multiline comment.
+Very good if you want to write long text,
+such as documentation, explainings, etc  ]]--
+
+print("hello world!")
+
+print("This function prints something on screen")
+
+print(
+    "But if you want to print long texts, "..
+    "I think is good to break in many lines "..
+    "and separate then with dot-dot '..'"
+)
